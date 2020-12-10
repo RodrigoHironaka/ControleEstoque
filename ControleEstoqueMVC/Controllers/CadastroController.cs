69921fa93969f6d4596f23524269b42661a92b39
Hironaka <Hironaka@DESKTOP-114H07D>
2020-12-09 21:02:36 -0300

    Aula 16

--- ControleEstoqueMVC/Controllers/CadastroController.cs
+++ ControleEstoqueMVC/Controllers/CadastroController.cs
@@ -10,7 +10,7 @@ namespace ControleEstoqueMVC.Controllers
     
     public class CadastroController : Controller
    {
-     
+        #region Grupos de produtos
         // GET: Cadastro
         [Authorize]
         public ActionResult GrupoProduto()
@@ -70,6 +70,76 @@ namespace ControleEstoqueMVC.Controllers
 
             return Json(new { Resultado = resultado, Mensagens = mensagens, IdSalvo = idSalvo });
         }
+        #endregion
+
+        #region Usuarios
+        private const string _senhaPadrao = "{$127;$188}";
+
+        [Authorize]
+        public ActionResult Usuario()
+        {
+            ViewBag.SenhaPadrao = _senhaPadrao;
+            return View(UsuarioModel.RecuperarLista());
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult RecuperarUsuario(int id)
+        {
+            return Json(UsuarioModel.RecuperarPeloId(id));
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult ExcluirUsuario(int id)
+        {
+            return Json(UsuarioModel.ExcluirPeloId(id));
+        }
+
+        [HttpPost]
+        [Authorize]
+        [ValidateAntiForgeryToken]
+        public ActionResult SalvarUsuario(UsuarioModel model)
+        {
+            var resultado = "OK";
+            var mensagens = new List<string>();
+            var idSalvo = string.Empty;
+
+            if (!ModelState.IsValid)
+            {
+                resultado = "AVISO";
+                mensagens = ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToList();
+            }
+            else
+            {
+                try
+                {
+                    if (model.Senha == _senhaPadrao)
+                    {
+                        model.Senha = "";
+                    }
+
+                    var id = model.Salvar();
+                    if (id > 0)
+                    {
+                        idSalvo = id.ToString();
+                    }
+                    else
+                    {
+                        resultado = "ERRO";
+                    }
+                }
+                catch (Exception ex)
+                {
+                    resultado = "ERRO";
+                }
+            }
+
+            return Json(new { Resultado = resultado, Mensagens = mensagens, IdSalvo = idSalvo });
+        }
+        #endregion
 
         [Authorize]
         public ActionResult MarcaProduto()
@@ -125,10 +195,5 @@ namespace ControleEstoqueMVC.Controllers
             return View();
         }
 
-        [Authorize]
-        public ActionResult Usuario()
-        {
-            return View();
-        }
     }
 }
